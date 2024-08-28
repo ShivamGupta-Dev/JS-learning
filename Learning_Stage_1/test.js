b57fa6c71659d1223aa732fa096df0ff9a235fb9
Shivam Gupta <155044334+ShivamGupta-Dev@users.noreply.github.com>
2024-08-28 11:13:47 +0000

    tables

--- Learning_Stage_1/test.js
+++ Learning_Stage_1/test.js
@@ -1,1 +1,9 @@
-console.log('Hello Shivam')
+const accountId = 21287700
+let accountEmail = 'shivam@12345'
+let password = 123456
+const accountVerified = 'True'
+
+console.log(accountEmail); //Comment Python<<<<<<<<JS 
+
+console.table([accountEmail, accountId, accountVerified, password]);
+
